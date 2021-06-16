bf3578fb6261cfbc15f51f78304bd2a64f2771cc
Maryna Mamalyga <maryna.mamaliga@gmail.com>
2021-06-16 18:30:23 +0200

    Task Manager - Improve the application console

--- src/main/java/com/company/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/company/taskmanager/TaskManagerApplication.java
@@ -6,40 +6,48 @@ import com.company.taskmanager.model.PriorityType;
 import com.company.taskmanager.model.Process;
 import com.company.taskmanager.service.impl.TaskManagerServiceImpl;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskManagerApplication {
+
   private static final TaskManagerServiceImpl taskqueue = new TaskManagerServiceImpl();
   private static boolean keepRunning = true;
 
-  public static void main(String[] args) throws TaskManagerException {
-    System.out.println("Starting the application ...");
+  public static void main(String[] args) {
+    System.out.println("Welcome to Task Manager");
+    System.out.println(
+        "To interact with the app introduce the NUMBER of the LISTED ACTIONS and press ENTER");
 
     while (keepRunning) {
-      Scanner scanner = new Scanner(new InputStreamReader(System.in));
-      System.out.println("Available actions");
-      System.out.println("1 - Add");
-      System.out.println("2 - List");
-      System.out.println("3 - Kill");
-      Integer operation = scanner.nextInt();
+      try {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        System.out.println("Available actions");
+        System.out.println("1 - Add");
+        System.out.println("2 - List");
+        System.out.println("3 - Kill");
+        System.out.println("4 - Exit the application");
+        Integer operation = scanner.nextInt();
 
-      if (operation == 1) {
-        manageAddOperation(scanner);
-      }
+        if (operation == 1) {
+          manageAddOperation(scanner);
+        } else if (operation == 2) {
+          manageListOperation(scanner);
+        } else if (operation == 3) {
+          manageKillOperation(scanner);
+        } else if (operation == 4) {
+          manageKillAppOperation();
+        } else {
+          System.out.println("This operation is not available");
+        }
 
-      if (operation == 2) {
-        manageListOperation(scanner);
+      } catch (InputMismatchException exception) {
+        System.out.println("The provided option is not available");
       }
-
-      if (operation == 3) {
-        manageKillOperation(scanner);
-      }
-      checkAndCloseApp(scanner);
     }
   }
 
-  private static void manageAddOperation(Scanner scanner) throws TaskManagerException {
+  private static void manageAddOperation(Scanner scanner) {
     System.out.println("Add version:");
     System.out.println("1 - regular");
     System.out.println("2 - fifo");
@@ -54,15 +62,26 @@ public class TaskManagerApplication {
 
     Process addedProcess;
     if (addVersion == 1) {
-      addedProcess  = taskqueue.add(priorityType);
+      try {
+        addedProcess = taskqueue.add(priorityType);
+        System.out.println("Added process: " + addedProcess);
+      } catch (TaskManagerException exception) {
+        System.out
+            .println("MAX CAPACITY achieved - no more elements can be added to the task manager");
+      }
     } else if (addVersion == 2) {
-      addedProcess  = taskqueue.addToFifo(priorityType);
+      addedProcess = taskqueue.addToFifo(priorityType);
+      System.out.println("Added process: " + addedProcess);
     } else if (addVersion == 3) {
-      addedProcess  = taskqueue.addWithPriority(priorityType);
+      addedProcess = taskqueue.addWithPriority(priorityType);
+      if (addedProcess == null) {
+        System.out.println("The process can not be inserted");
+      } else {
+        System.out.println("Added process: " + addedProcess);
+      }
     } else {
-      throw new UnsupportedOperationException("The specified add version is not supported");
+      System.out.println("The specified add version is not supported");
     }
-    System.out.println("Added process: "+addedProcess);
   }
 
   private static void manageListOperation(Scanner scanner) {
@@ -72,14 +91,20 @@ public class TaskManagerApplication {
     System.out.println("3 - Id");
 
     Integer listVersion = scanner.nextInt();
-    SortingType sortingType = SortingType.CREATION_TIME;
-    if (listVersion == 2) {
+    SortingType sortingType;
+
+    if (listVersion == 1) {
+      sortingType = SortingType.CREATION_TIME;
+      System.out.println(taskqueue.listAll(sortingType).toString());
+    } else if (listVersion == 2) {
       sortingType = SortingType.PRIORITY;
+      System.out.println(taskqueue.listAll(sortingType).toString());
     } else if (listVersion == 3) {
       sortingType = SortingType.ID;
+      System.out.println(taskqueue.listAll(sortingType).toString());
+    } else {
+      System.out.println("The specified list version is not supported");
     }
-    List<Process> sortedProcesses = taskqueue.listAll(sortingType);
-    System.out.println(sortedProcesses.toString());
   }
 
   private static void manageKillOperation(Scanner scanner) {
@@ -93,30 +118,31 @@ public class TaskManagerApplication {
       Integer pid = scanner.nextInt();
       taskqueue.kill(pid);
     } else if (killVersion == 2) {
+      System.out.println("Insert a priority");
+      System.out.println("1 - low");
+      System.out.println("2 - medium");
+      System.out.println("3 - high");
       Integer priority = scanner.nextInt();
       taskqueue.killAll(getPriorityType(priority));
     } else if (killVersion == 3) {
       taskqueue.killAll();
+    } else {
+      System.out.println("The specified kill version is not supported");
     }
   }
 
   private static PriorityType getPriorityType(Integer processPriority) {
     PriorityType priorityType = PriorityType.LOW;
-    if(processPriority == 2) {
+    if (processPriority == 2) {
       priorityType = PriorityType.MEDIUM;
-    } else if(processPriority == 3){
+    } else if (processPriority == 3) {
       priorityType = PriorityType.HIGH;
     }
     return priorityType;
   }
 
-  private static void checkAndCloseApp(Scanner scanner) {
-    System.out.println("Exit the application?");
-    System.out.println("1 - Yes");
-    System.out.println("2 - No?");
-    Integer exitAppOption = scanner.nextInt();
-    if(exitAppOption == 1) {
-      keepRunning = false;
-    }
+  private static void manageKillAppOperation() {
+    keepRunning = false;
+    System.exit(0);
   }
 }
